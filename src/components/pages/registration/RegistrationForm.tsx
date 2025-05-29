88cca73f7ff128006cb7e5fbdc2cb3131fe74e2e
Andrey22 <andreymail22112006@gmail.com>
2025-05-29 15:04:22 +0300

    feat: #3 Добавить форму регистрации

---
+++ src/components/pages/registration/RegistrationForm.tsx
@@ -0,0 +1,72 @@
+import { NavLink } from 'react-router-dom';
+import classes from './RegistrationForm.module.scss';
+import cn from 'classnames';
+import { Field } from '../../common/Field/Field';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '../../common/ErrorMessage/ErrorMessage';
+import { PasswordInput } from '../../common/PasswordInput/PasswordInput';
+
+type FormData = {
+    login: string;
+    password: string;
+};
+
+const RegistrationForm: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, dirtyFields },
+    } = useForm<FormData>();
+
+    const onSubmit = (formData: FormData) => {
+        console.log(formData);
+    };
+
+    return (
+        <section className="container" onSubmit={handleSubmit(onSubmit)}>
+            <form className={classes.form}>
+                <h1 className={classes.title}>Регистрация</h1>
+                <div className="form-block">
+                    <label htmlFor="login">Логин</label>
+                    <Field
+                        id="login"
+                        type="text"
+                        name="login"
+                        register={register}
+                        options={{
+                            required: 'Это поле обязательное',
+                        }}
+                    />
+                    <ErrorMessage error={errors.login} />
+                </div>
+                <div className="form-block">
+                    <label htmlFor="password">Пароль</label>
+                    <PasswordInput
+                        id="password"
+                        name="password"
+                        register={register}
+                        options={{
+                            required: 'Это поле является обязательным',
+                        }}
+                    />
+                    <ErrorMessage error={errors.login} />
+                </div>
+                <button className="button-primary">Зарегистрироваться</button>
+                <div className={classes.bottom}>
+                    <p className="text-primary">Уже есть аккаунт?</p>
+                    <NavLink
+                        to={'/auth'}
+                        className={cn(
+                            'button-primary-outlined',
+                            classes.registrationButton,
+                        )}
+                    >
+                        Войти
+                    </NavLink>
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default RegistrationForm;
